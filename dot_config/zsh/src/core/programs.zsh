707839e93f3de3f59a105e6d76b1586f7a47a9ec
ikkair <ikhkair@gmail.com>
2024-03-22 23:29:37 +0700

    Fix zellij initialization

--- dot_config/zsh/src/core/programs.zsh
+++ dot_config/zsh/src/core/programs.zsh
@@ -1,7 +1,21 @@
 if (( $+commands[zellij] )); then
-  eval "$(zellij setup --generate-auto-start zsh)"
+  if [[ -n $XDG_CURRENT_DESKTOP ]] && [[ -z "$ZELLIJ" ]]; then
+    if zellij list-sessions | rg -q 'staff.*EXITED'; then
+      zellij attach staff
+    elif zellij list-sessions | rg -q 'staff'; then
+      zellij
+    else
+      zellij attach -c staff
+    fi
+  fi
 else
   echo 'zellij: command not found'
+fi
+
+if (( $+commands[atuin] )); then
+  eval "$(atuin init zsh)"
+else
+  echo 'atuin: command not found'
 fi
 
 if (( $+commands[zoxide] )); then
