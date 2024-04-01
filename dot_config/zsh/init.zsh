87e0228824055c2886017fc55a79d15836a05985
ikkair <ikhkair@gmail.com>
2024-04-01 23:49:08 +0700

    Adding keybinds for ZLE

--- dot_config/zsh/init.zsh
+++ dot_config/zsh/init.zsh
@@ -3,6 +3,7 @@ CONFIG_PATH_ZSH="$(dirname "$0")"
 # Cores
 source $CONFIG_PATH_ZSH/src/core/path.zsh
 source $CONFIG_PATH_ZSH/src/core/env.zsh
+source $CONFIG_PATH_ZSH/src/core/keybind.zsh
 source $CONFIG_PATH_ZSH/src/core/programs.zsh
 
 # Plugins
---
+++ dot_config/zsh/src/core/keybind.zsh
@@ -0,0 +1,27 @@
+
+# Home and End key
+bindkey -M emacs "^[[H" beginning-of-line
+bindkey -M emacs "^[[F" end-of-line
+bindkey -M viins "^[[H" beginning-of-line
+bindkey -M viins "^[[F" end-of-line
+bindkey -M vicmd "^[[H" beginning-of-line
+bindkey -M vicmd "^[[F" end-of-line
+
+# Ctrl Left and Right Key
+bindkey -M emacs "^[[1;5D" vi-backward-word
+bindkey -M emacs "^[[1;5C" vi-forward-word-end
+bindkey -M viins "^[[1;5D" vi-backward-word
+bindkey -M viins "^[[1;5C" vi-forward-word-end
+bindkey -M vicmd "^[[1;5D" vi-backward-word
+bindkey -M vicmd "^[[1;5C" vi-forward-word-end
+
+# Ctrl Backspace to delete a word
+bindkey -M emacs "^H" backward-kill-word
+bindkey -M viins -r "^H"
+bindkey -M viins "^H" backward-kill-word
+
+# Esc to vicmd mode from emacs
+bindkey -M emacs "\e" vi-cmd-mode
+
+# To use normal backspace in vi insert
+bindkey -M viins "\b" backward-delete-char
